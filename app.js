3f1523370604da9cb8438aea14998af58a2881a6
Dev simeon <elebesunuosereme@gmail.com>
2023-12-15 20:09:36 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const primaryHeader = document.querySelector('.primary-header');
+const primaryNav = document.querySelector('.primary-navigation');
+const closeIcon = document.querySelector('.icon-close');
+const menuIcon = document.querySelector('.menu-icon');
+const dropdownItems = document.querySelectorAll('.dropdown-item');
+
+menuIcon.addEventListener('click',()=>{
+    primaryNav.toggleAttribute('data-visible')
+
+   
+
+    menuIcon.setAttribute('aria-expanded',true);
+    closeIcon.setAttribute('aria-expanded',false);
+    primaryHeader.toggleAttribute('data-overlay')
+
+})
+closeIcon.addEventListener('click',()=>{
+    primaryNav.toggleAttribute('data-visible');
+
+    menuIcon.setAttribute('aria-expanded',false);
+    closeIcon.setAttribute('aria-expanded',true);
+    primaryHeader.toggleAttribute('data-overlay')
+
+})
+
+dropdownItems.forEach((item)=>{
+    // item.addEventListener('click',()=>{
+    //     item.classList.toggle('active')
+    // })
+   const itemName = item.querySelector('.dropdown-item__name');
+    itemName.addEventListener('click',()=>{
+        item.classList.toggle('active');
+    })
+
+    closeIcon.addEventListener('click',()=>{
+        item.classList.remove('active')
+    })
+})
